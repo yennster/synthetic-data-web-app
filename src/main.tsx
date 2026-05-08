d70be4f1bab5dc0640b64cf8981b06ae74ee7fe5
Jenny Speelman <jenny@edgeimpulse.com>
2026-05-08 10:23:37 +0200

    Auto-fill API key from ?apiKey= URL param and post iframe height to parent

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import { useStore } from './store/useStore';
+
+function applyApiKeyFromUrl() {
+  const apiKey = new URLSearchParams(window.location.search).get('apiKey');
+  if (apiKey) useStore.getState().setEi({ apiKey });
+}
+
+function initPostContentHeight() {
+  function postHeight() {
+    const height = document.body.scrollHeight;
+    console.log('posting', height);
+    window.parent.postMessage({ type: 'IFRAME_HEIGHT', height }, '*');
+  }
+
+  window.addEventListener('load', postHeight);
+  window.addEventListener('resize', postHeight);
+
+  const ro = new ResizeObserver(() => postHeight());
+  ro.observe(document.body);
+
+  postHeight();
+}
+
+applyApiKeyFromUrl();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+initPostContentHeight();
